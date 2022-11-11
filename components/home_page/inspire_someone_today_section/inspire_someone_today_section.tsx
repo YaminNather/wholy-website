4210419d91a11854d12a5b9a35c914af564bbe98
Yamin Nather <2001s.yn@gmail.com>
2022-11-12 00:55:35 +0530

    Fixed inspire me section being reactive after the home page animation is done

--- components/home_page/inspire_someone_today_section/inspire_someone_today_section.tsx
+++ components/home_page/inspire_someone_today_section/inspire_someone_today_section.tsx
@@ -6,51 +6,63 @@ import backgroundImage from "../../../public/yellow-textured-background.jpeg";
 import paintStrokesImage from "../../../public/home/paint-strokes.png";
 import classNames from "classnames";
 
-const texts: string[] = [
-    "Go on. Inspire someone today.",
-    "Workout.",
-    "Meditate.",
-    "Run.",
-    "Change.",
-    "Treat Themselves.",
-    "Check out Wholy"
+const texts: string[] = [    
+    "workout",
+    "meditate",
+    "run",
+    "change",
+    "treat themselves",
+    "check out Wholy"
 ];
 
 export const InspireSomeoneTodaySection: FC = () => {
     const [isOpen, setIsOpen] = useState<boolean>(true);
     const [currentTextIndex, setCurrentTextIndex] = useState<number>(0);
 
+    const [isRemoved, setIsRemoved] = useState<boolean>(false);
+
     return (
         <section 
             id="hero-section" 
             className={styles.inspire_someone_today_section} 
-            style={{animation: (!isOpen) ? `${styles.page_peel} 1000ms forwards` : "none"}}
+            style={{
+                display: (isRemoved) ? "none" : "revert",
+                animation: (!isOpen) ? `${styles.page_peel} 1000ms forwards` : "none"
+            }}
+            onAnimationEnd={(event) => {
+                if(event.animationName === styles.page_peel) setIsRemoved(true);
+            }}
         >
             <div className={styles.main_content}>
                 <Image src={backgroundImage} alt={""} className="background_image" />
                 
                 <div className={classNames("container", styles.container)}>
+                <p>
+                    Go on inspire someone to
                     {texts.map(
                         (text, index, array) => {
                             return (
-                                <p
+                                <span
                                     key={index} 
                                     style={{display: (currentTextIndex === index) ? "revert" : "none"}}
                                     onAnimationEnd={(event) => {
                                         console.log(`CustomLog: Animation on text[${index}] ended`);
-
+                
                                         if(currentTextIndex < texts.length - 1)setCurrentTextIndex(currentTextIndex + 1);
                                         else setIsOpen(false);
                                     }}
                                 >
-                                    {text}
-                                </p>
+                                    &nbsp;{text}
+                                </span>
                             );
                         }
                     )}
+                </p>                    
                     
                     <div className={styles.bottom_area}>
                         <Image src={paintStrokesImage} alt="" className={styles.paint_strokes} />
+
+                        <p>today</p>
                     </div>
                 </div>            
 
