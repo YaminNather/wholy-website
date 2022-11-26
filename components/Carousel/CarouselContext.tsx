66a373f154ab96be6dfe36ed39c2956af7f0aa50
YaminNather <2001s.yn@gmail.com>
2022-11-26 16:20:44 +0530

    Carousel

--- components/Carousel/CarouselContext.tsx
+++ components/Carousel/CarouselContext.tsx
@@ -1,4 +1,4 @@
 import { Context, createContext } from "react";
 import { CarouselData } from "./CarouselData";
 
-export const carouselContext: Context<CarouselData> = createContext<CarouselData>({currentSlide: 0});
+export const carouselContext: Context<CarouselData> = createContext<CarouselData>({currentSlide: 0, isMouseHovering: false});
