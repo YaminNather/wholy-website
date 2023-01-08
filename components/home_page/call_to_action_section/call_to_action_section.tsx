b52b1588470b955d09ef9107e6016c42d9bc035a
YaminNather <2001s.yn@gmail.com>
2023-01-08 23:20:50 +0530

    added multicolored lines

--- components/home_page/call_to_action_section/call_to_action_section.tsx
+++ components/home_page/call_to_action_section/call_to_action_section.tsx
@@ -4,14 +4,14 @@ import styles from "./call_to_action_section_styles.module.scss";
 import { UIProducts } from "../../../product_ui_details/ui_products";
 import { ProductCard } from "./product_card/product_card";
 import classNames from "classnames";
-import { instagramLogoVector } from "../../../common_imported_images/social_media_logos";
+import multiColoredWavesImage from "../../../public/home/call-to-action/multi-colored-waves.png";
 
 export const CallToActionSection: FC = (props) => {
     return (
         <section id="call_to_action_section" className={classNames("light_theme", styles.section)}>
-            <div className={classNames("container", styles.container)}>
-                <h1>Now that we got your attention, Go on! <strong>Grab a bite!</strong></h1>
+            <h1>Now that we got your attention, Go on! <strong>Grab a bite!</strong></h1>
 
+            <div className={styles.grid_area}>
                 <div className={styles.grid}>
                     {UIProducts.array.map(
                         (value, index, array) => {
@@ -21,21 +21,23 @@ export const CallToActionSection: FC = (props) => {
                         }
                     )}
                 </div>
-
-                <button>SHOP OUR RANGE</button>
-
-                {/* <div className={styles.heading_area}>
-                    <h1>We dont mind you stalking us!</h1>
-
-                    <div className={styles.instagram_handle_area}>
-                        <div className={styles.logo_container}>
-                            <Image src={instagramLogoVector} alt="" />
-                        </div>
-                        
-                        <p>@insta_handle</p>
-                    </div>
-                </div> */}
+                
+                <Image src={multiColoredWavesImage} alt="" className={styles.multi_colored_waves} />
             </div>
+
+            <button>SHOP OUR RANGE</button>
+
+            {/* <div className={styles.heading_area}>
+                <h1>We dont mind you stalking us!</h1>
+
+                <div className={styles.instagram_handle_area}>
+                    <div className={styles.logo_container}>
+                        <Image src={instagramLogoVector} alt="" />
+                    </div>
+                    
+                    <p>@insta_handle</p>
+                </div>
+            </div> */}
         </section>
     );
 };
