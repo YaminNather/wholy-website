7264dadbecb5b34c2fb7a9d5eb01005bd00b618c
YaminNather <2001s.yn@gmail.com>
2023-02-15 11:12:49 +0530

    Added environment variables to next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ const nextConfig = {
     domains: [
       "firebasestorage.googleapis.com"
     ]
+  },
+  env: {
+    NEXT_PUBLIC_RAZORPAY_API_KEY: process.env["NEXT_PUBLIC_RAZORPAY_API_KEY"],
+    RAZORPAY_SECRET: process.env["RAZORPAY_SECRET"],
+    SHIPROCKET_EMAIL: process.env["SHIPROCKET_EMAIL"],
+    SHIPROCKET_PASSWORD: process.env["SHIPROCKET_PASSWORD"]
   }
 }
 
