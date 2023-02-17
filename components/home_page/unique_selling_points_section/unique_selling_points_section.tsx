50299d8419db4387b7a506ae870b8746ad2efced
YaminNather <2001s.yn@gmail.com>
2023-02-17 18:51:57 +0530

    Fixed UniqueSellingPointsArea scss import bugs

--- components/home_page/unique_selling_points_section/unique_selling_points_section.tsx
+++ components/home_page/unique_selling_points_section/unique_selling_points_section.tsx
@@ -8,6 +8,7 @@ import uniqueSellingPointsImage1 from "../../../public/home/unique-selling-point
 import uniqueSellingPointsImage2 from "../../../public/home/unique-selling-points/2.png";
 import uniqueSellingPointsImage3 from "../../../public/home/unique-selling-points/3.png";
 import greenBoxImage from "../../../public/home/unique-selling-points/green-box.png";
+import { UniqueSellingPointsArea } from "../../common/unique_selling_points_area/unique_selling_points_area";
 
 const uniqueSellingPointsImages: StaticImageData[] = [
     uniqueSellingPointsImage0,
@@ -21,30 +22,7 @@ export const UniqueSellingPointsSection: FC = (props) => {
         <section id="unique_selling_points_section" className={classNames("light_theme", styles.section)}>
             <Image src={yellowTexturedBackgroundImage} alt="" className={"background_image"} />
 
-            <div className={classNames("container", styles.container)}>
-                <h1>Baked with real stuff found in your kitchen!</h1>
-
-                <hr />
-
-                <div className={styles.grid}>
-                    {uniqueSellingPointsImages.map((value, index, array) => <Image key={index} src={value} alt="" />)}
-                </div>
-
-                <div className={styles.green_box}>
-                    <Image src={greenBoxImage} alt="" />
-
-                    Stuff not found in your
-                    <span className={styles.correction_area}>
-                        <span className={classNames("personalized_text", styles.correction)}>and our</span>
-                        
-                        <span className={classNames("personalized_text", styles.correction_indicator)}>{"^"}</span>
-
-                        &nbsp;kitchen
-                    </span>
-                </div>
-
-                <p>No Maida | No Refined Sugar | No Preservatives | No Trans Fat | No Artificial Color or Flavour</p>
-            </div>
+            <UniqueSellingPointsArea />
         </section>
     );
 };
