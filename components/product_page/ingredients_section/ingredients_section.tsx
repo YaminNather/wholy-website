92cf07bd0e28360900cdbc1927e00e2551db4b17
YaminNather <2001s.yn@gmail.com>
2023-02-18 17:27:19 +0530

    Made Product Page's Ingredients Section responsive

--- components/product_page/ingredients_section/ingredients_section.tsx
+++ components/product_page/ingredients_section/ingredients_section.tsx
@@ -5,62 +5,89 @@ import styles from "./ingredients_section_styles.module.scss";
 import classNames from "classnames";
 import { UIProducts } from "../../../product_ui_details/ui_products";
 
+import doodleImage from "../../../public/product/doodle.png";
+
 import grainBowlImage from "../../../public/product/grain-bowl.png";
 import datesBowlImage from "../../../public/product/dates-bowl.png";
 
 import peanutsImage from "../../../public/product/peanuts.png";
+import almondsImage from "../../../public/product/almonds.png";
 
 export const IngredientsSection: FC = (props) => {
     return (
         <section style={{backgroundColor: UIProducts.strawberry.color}} className={classNames(styles.ingredients_section)}>
             <div className={classNames("container", styles.container)}>
-                <div className={classNames(styles.cell, styles.oats_details_cell)}>
-                    <Image src={grainBowlImage} alt="" />
+                <div className={styles.left_cell}>
+                    <div className={classNames(styles.cell, styles.oats_details_cell)}>
+                        <div className={styles.ingredient_in_bowl_image_container}>
+                            <Image src={grainBowlImage} alt="" className={styles.image} />
 
-                    <div className={styles.text_area}>
-                        <h2>Oats Flour or Oats</h2>
+                            <Image src={doodleImage} alt="" className={styles.doodle} />
+                        </div>
 
-                        <p className={"personalized_text"}>
-                            Baked with hand milled oats flour. Rich in <strong>anti oxidants and beta glucan</strong> for heart health,
-                            gut health, gut health & cholestrol reduction.
-                        </p>
-                    </div>
-                </div>
+                        <div className={styles.text_area}>
+                            <h2>Oats Flour or Oats</h2>
 
-                <Image src={UIProducts.strawberry.wrappedCookiePortraitImage} alt="" className={styles.cookie} />
-                
-                <div className={classNames(styles.cell, styles.peanuts_details_cell)}>
-                    <Image src={peanutsImage} alt="" className={styles.peanuts_image} />
-                    <div className={styles.text_area}>
-                        <h2>Peanuts</h2>
-
-                        <p className={"personalized_text"}>
-                            Peanuts are <strong>certified super food</strong>. 
-                            Our primary source of protein and healthy fats for all day plant powered energy.
-                        </p>
-                    </div>
-                </div>
-                
-                <div className={classNames(styles.cell, styles.dates_details_cell)}>
-                    <div className={styles.text_area}>
-                        <h2>Dates</h2>
-
-                        <p className={"personalized_text"}>
-                            Dates are packed with iron to <strong>boost your brain health</strong>.
-                        </p>
+                            <p className={"personalized_text"}>
+                                Baked with hand milled oats flour. Rich in <strong>anti oxidants and beta glucan</strong> for heart health,
+                                gut health, gut health & cholestrol reduction.
+                            </p>
+                        </div>
                     </div>
                     
-                    <Image src={datesBowlImage} alt="" />
-                </div>
-                
-                <div className={styles.almonds_details_cell}>
-                    <div className={styles.text_area}>
-                        <h2>Almonds, chia seeds and flax seeds</h2>
+                    <div className={classNames(styles.cell, styles.peanuts_details_cell)}>
+                        <Image src={peanutsImage} alt="" className={styles.peanuts_image} />
+                        <div className={styles.text_area}>
+                            <h2>Peanuts</h2>
 
-                        <p className={"personalized_text"}>
-                            Topped with <strong>heart healthy nuts & seeds</strong> for that extra antioxidant
-                            and nutrient boost
-                        </p>
+                            <p className={"personalized_text"}>
+                                Peanuts are <strong>certified super food</strong>. 
+                                Our primary source of protein and healthy fats for all day plant powered energy.
+                            </p>
+                        </div>
+                    </div>
+                    
+                    <div className={classNames(styles.cell, styles.dates_details_cell)}>
+                        <div className={styles.text_area}>
+                            <h2>Dates</h2>
+
+                            <p className={"personalized_text"}>
+                                Dates are packed with iron to <strong>boost your brain health</strong>.
+                            </p>
+                        </div>
+                        
+                        <div className={styles.ingredient_in_bowl_image_container}>
+                            <Image src={datesBowlImage} alt="" className={styles.image} />
+
+                            <Image src={doodleImage} alt="" className={styles.doodle} />
+                        </div>
+                    </div>
+                    
+                    <div className={styles.almonds_details_cell}>
+                        <div className={styles.text_area}>
+                            <div className={styles.heading}>
+                                <h2>
+                                    Almonds, chia seeds <br/>and flax seeds.
+                                </h2>
+
+                                <Image src={almondsImage} alt="" />
+                            </div>
+
+                            <p className={"personalized_text"}>
+                                Topped with <strong>heart healthy nuts & seeds</strong> for that extra antioxidant
+                                and nutrient boost
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.right_cell}>
+                    <Image src={UIProducts.strawberry.wrappedCookiePortraitImage} alt="" className={styles.cookie} />
+
+                    <div className={styles.get_energy_reboost_area}>
+                        <p>Energy Reboost</p>
+                        
+                        <button className={"button_yellow"}>GET YOURS</button>
                     </div>
                 </div>
             </div>
