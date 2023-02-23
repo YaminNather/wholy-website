7c2324e11ea491e5505c0c7b4797bbcc1c33157a
YaminNather <2001s.yn@gmail.com>
2023-02-23 22:59:15 +0530

    Changed response limit in NextJS config.

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
       "firebasestorage.googleapis.com"
     ]
   },
+  api: {
+    responseLimit: "100mb",
+  },
   env: {
     NEXT_PUBLIC_RAZORPAY_API_KEY: process.env["NEXT_PUBLIC_RAZORPAY_API_KEY"],
     RAZORPAY_SECRET: process.env["RAZORPAY_SECRET"],
