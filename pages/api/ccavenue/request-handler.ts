955fbfe5a119f67f0eeb8b9046a54ec94bedf2ea
YaminNather <2001s.yn@gmail.com>
2023-03-05 14:15:33 +0530

    debugging

--- pages/api/ccavenue/request-handler.ts
+++ pages/api/ccavenue/request-handler.ts
@@ -26,7 +26,7 @@ export default async function(request: NextApiRequest, response: NextApiResponse
         // debugResponseData += "\nDone Parsing queries";
         // debugResponseData += `\nPost Data:\n${JSON.stringify(post, null, 2)}`;
 
-        const iframeUrl: URL = new URL("/transaction/transcation.do", "https://secure.ccavenue.com");
+        const iframeUrl: URL = new URL("/transaction/transaction.do", "https://secure.ccavenue.com");
         //command=initiateTransaction&merchant_id='+request.body.merchant_id+'&encRequest='+encryptRequest+'&access_code='+Env.ccAvenueAccessCode
         iframeUrl.searchParams.append("command", "initiateTransaction");
         iframeUrl.searchParams.append("merchant_id", request.body.merchant_id);
