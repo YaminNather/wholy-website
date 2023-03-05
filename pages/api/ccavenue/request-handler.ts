e311350e40b25f907969ad6eb9ea11d74beb1cbf
YaminNather <2001s.yn@gmail.com>
2023-03-05 13:20:59 +0530

    Debugging

--- pages/api/ccavenue/request-handler.ts
+++ pages/api/ccavenue/request-handler.ts
@@ -23,21 +23,21 @@ export default async function(request: NextApiRequest, response: NextApiResponse
             return;
         }
         debugResponseData += "\nDone encrypting";
-        const post: ParsedUrlQuery = querystring.parse(body);
-        debugResponseData += "\nDone Parsing queries";
-        debugResponseData += `\nPost Data:\n${JSON.stringify(post, null, 2)}`;
+        // const post: ParsedUrlQuery = querystring.parse(body);
+        // debugResponseData += "\nDone Parsing queries";
+        // debugResponseData += `\nPost Data:\n${JSON.stringify(post, null, 2)}`;
 
-        const formBody: string = '<html><head><title>Sub-merchant checkout page</title><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script></head><body><center><!-- width required mininmum 482px --><iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id='+post.merchant_id+'&encRequest='+encryptRequest+'&access_code='+Env.ccAvenueAccessCode+'"></iframe></center><script type="text/javascript">$(document).ready(function(){$("iframe#paymentFrame").load(function() {window.addEventListener("message", function(e) {$("#paymentFrame").css("height",e.data["newHeight"]+"px"); }, false);}); });</script></body></html>';    
+        const formBody: string = '<html><head><title>Sub-merchant checkout page</title><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script></head><body><center><!-- width required mininmum 482px --><iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id='+request.body.merchant_id+'&encRequest='+encryptRequest+'&access_code='+Env.ccAvenueAccessCode+'"></iframe></center><script type="text/javascript">$(document).ready(function(){$("iframe#paymentFrame").load(function() {window.addEventListener("message", function(e) {$("#paymentFrame").css("height",e.data["newHeight"]+"px"); }, false);}); });</script></body></html>';    
         debugResponseData += "\nDone initializing formBody";
         debugResponseData += `\nForm body: ${formBody}`;
 
-        // const responseHeaders: { [key: string]: string } = {
-        //     "Content-Type": "text/html"
-        // };
-        // response.setHeader("Content-Type", "text/html");
-        // response.send(formBody);
+        const responseHeaders: { [key: string]: string } = {
+            "Content-Type": "text/html"
+        };
+        response.setHeader("Content-Type", "text/html");
+        response.send(formBody);
 
-        response.send(`Debug response data:\n${debugResponseData}`);
+        // response.send(`Debug response data:\n${debugResponseData}`);
     }
     catch(e) {
         const exception: any = e;
