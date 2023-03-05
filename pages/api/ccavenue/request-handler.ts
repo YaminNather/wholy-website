40e1b7df426fa8673d59df251d9496b378bef1f3
YaminNather <2001s.yn@gmail.com>
2023-03-05 12:28:40 +0530

    Debug

--- pages/api/ccavenue/request-handler.ts
+++ pages/api/ccavenue/request-handler.ts
@@ -6,6 +6,8 @@ import { Env } from "../../../env";
 import ccAvenueUtils from "../../../ccavenue/ccavutil.js";
 
 export default async function(request: NextApiRequest, response: NextApiResponse) {
+    let debugResponseData: string = "";
+
     const body: string = JSON.stringify(request.body, null, 2);
     const workingKey: string = Env.ccAvenueWorkingKey;
     let encryptRequest;
@@ -14,16 +16,23 @@ export default async function(request: NextApiRequest, response: NextApiResponse
     }
     catch(e) {
         const exception: any = e;
-        response.send(`Name: ${exception.name}\nMessage: ${exception.message}\nStack: ${exception.stack}`);
+        debugResponseData += `\nName: ${exception.name}\nMessage: ${exception.message}\nStack: ${exception.stack}`
+        response.send(debugResponseData);
         return;
     }
+    debugResponseData += "\nDone encrypting";
     const post: ParsedUrlQuery = querystring.parse(body);
+    debugResponseData += `\nPost Data:\n${post}`;
 
-    const formBody: string = '<html><head><title>Sub-merchant checkout page</title><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script></head><body><center><!-- width required mininmum 482px --><iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id='+post.merchant_id+'&encRequest='+encryptRequest+'&access_code='+Env.ccAvenueAccessCode+'"></iframe></center><script type="text/javascript">$(document).ready(function(){$("iframe#paymentFrame").load(function() {window.addEventListener("message", function(e) {$("#paymentFrame").css("height",e.data["newHeight"]+"px"); }, false);}); });</script></body></html>';
+    const formBody: string = '<html><head><title>Sub-merchant checkout page</title><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script></head><body><center><!-- width required mininmum 482px --><iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id='+post.merchant_id+'&encRequest='+encryptRequest+'&access_code='+Env.ccAvenueAccessCode+'"></iframe></center><script type="text/javascript">$(document).ready(function(){$("iframe#paymentFrame").load(function() {window.addEventListener("message", function(e) {$("#paymentFrame").css("height",e.data["newHeight"]+"px"); }, false);}); });</script></body></html>';    
 
-    const responseHeaders: { [key: string]: string } = {
-        "Content-Type": "text/html"
-    };
-    response.setHeader("Content-Type", "text/html");
-    response.send(formBody);
+    debugResponseData += `\nForm body: ${formBody}`;
+
+    // const responseHeaders: { [key: string]: string } = {
+    //     "Content-Type": "text/html"
+    // };
+    // response.setHeader("Content-Type", "text/html");
+    // response.send(formBody);
+
+    response.send(`Debug response data:\n${debugResponseData}`);
 }
