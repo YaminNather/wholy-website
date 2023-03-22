3b8a19cb7e229dbdcf8551422345d25aefa67b58
Yamin Nather <2001s.yn@gmail.com>
2023-03-22 07:55:47 +0530

    Added google tag manager tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { TagManagerTag } from '../components/common/google_tag_manager/tag_manager_tag'
 
 export default function Document() {
   return (
     <Html style={{scrollBehavior: "smooth"}}>
         <Head>
+            <TagManagerTag />
+
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
             {/* <link href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap" rel="stylesheet" /> */}
@@ -12,6 +15,14 @@ export default function Document() {
         </Head>
       
         <body style={{position: "relative"}}>
+            <noscript>
+                <iframe 
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-MPR3T85" 
+                    height="0" width="0"
+                    style={{ display: "none", visibility: "hidden" }}
+                />
+            </noscript>
+
             <Main />
             
             <NextScript />
