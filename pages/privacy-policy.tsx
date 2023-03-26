10e197291aade098548f1598a530f2fb59cc5d4c
YaminNather <2001s.yn@gmail.com>
2023-03-26 21:40:47 +0530

    Added the missing information in privacy-policy page

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -22,6 +22,31 @@ const PrivacyPolicy: FC = (props) => {
             <p>
                 We use the information we collect to provide and improve our website and services, to communicate with you about our products and services, and to personalize your experience on our website.
             </p>
+
+            <h2>Sharing of Information</h2> 
+            
+            <p>
+                We may share your information with third-party service providers that assist us in providing our website and services, such as payment processors and shipping providers. We may also share your information with law enforcement agencies or other third parties if we believe that disclosure is necessary to comply with a legal obligation or to protect our rights or the rights of others.
+            </p>
+
+            <h2>Data Retention</h2>
+            
+            <p>
+                We will retain your information for as long as necessary to provide our website and services to you, or as required by applicable laws or regulations.
+            </p>
+
+            <h2>Your Rights</h2>
+            
+            <p>
+                You have the right to access, modify, or delete your personal information. You can also object to the processing of your personal information, or request that we restrict the processing of your personal information.
+            </p>
+
+            <h2>Contact Us</h2> 
+            
+            <p>
+                If you have any questions or concerns about our Privacy Policy, please contact us at hello@eatwholy.com.
+            </p>
+
         </LegalDetailsPage>
     );
 };
